65338bc0a330835bb951710abe71c8b2beb08e39
bego99 <begoja@edu.aau.at>
2021-04-29 20:19:22 +0200

    createted CreateSession View + buttons

---
+++ app/src/main/java/com/game/chessmate/createSession.java
@@ -0,0 +1,14 @@
+package com.game.chessmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createSession extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_session);
+    }
+}

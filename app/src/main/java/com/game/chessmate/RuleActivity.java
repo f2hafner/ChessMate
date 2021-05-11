2fd4ac8aa98ec0933062194c4b66cb46620c4b8c
bego99 <begoja@edu.aau.at>
2021-05-11 13:51:55 +0200

    OptionsScreen: Changed Layout and Buttons

--- app/src/main/java/com/game/chessmate/RuleActivity.java
+++ app/src/main/java/com/game/chessmate/RuleActivity.java
@@ -13,6 +13,7 @@ public class RuleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+        getSupportActionBar().hide();
 
 
         Button back = findViewById(R.id.backButton);

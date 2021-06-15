69b8dc45548705cb822eb14f769fe65152e8d160
VeronikaSemmelrock <v1semmelro@edu.aau.at>
2021-06-15 14:41:27 +0200

    Adapted Queen to only allow a legal move if move does not cause king to be in check.

--- app/src/main/java/com/game/chessmate/GameFiles/PlayingPieces/Queen.java
+++ app/src/main/java/com/game/chessmate/GameFiles/PlayingPieces/Queen.java
@@ -44,12 +44,17 @@ public class Queen extends ChessPiece {
                 if(!(i == currentPosition.getFieldX() && j == currentPosition.getFieldY()) && !opponentEncountered){
                     if (currentFields[i][j].getCurrentPiece() == null) {
                         if(!currentFields[i][j].isBlocked())
-                             legalFields.add(currentFields[i][j]);
+                            if(!wouldbeChecked(currentFields, currentFields[i][j])){//piece can only move to legal field if it does not cause the king to be in check
+                                legalFields.add(currentFields[i][j]);
+                            }
                         else
                             break;
                     } else if (currentFields[i][j].getCurrentPiece().getColour() != this.colour&&!currentFields[i][j].isProtected()) {
-                        legalFields.add(currentFields[i][j]);
+                        if(!wouldbeChecked(currentFields, currentFields[i][j])){//piece can only move to legal field if it does not cause the king to be in check
+                            legalFields.add(currentFields[i][j]);
+                        }
                         opponentEncountered = true;
+
                     }else{
                         break;
                     }
